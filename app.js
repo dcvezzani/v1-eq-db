7b51d960f2cf0fbf5540d3356d5080f7e82370a4
David Vezzani <dcvezzani@ldschurch.org>
2018-10-01 06:31:29 -0600

    adds cool

--- app.js
+++ app.js
@@ -1,3 +1,4 @@
+const cool = require('cool-ascii-faces')
 var createError = require('http-errors');
 var express = require('express');
 var path = require('path');
@@ -17,7 +18,8 @@ app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, 'public')))
+  .get('/cool', (req, res) => res.send(cool()));
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
