49db114ebb1b57ecefd9ce5fefcf214dd1959ae2
David Vezzani <dcvezzani@ldschurch.org>
2018-10-01 06:53:03 -0600

    respond with json

--- app.js
+++ app.js
@@ -30,7 +30,7 @@ app.use(express.static(path.join(__dirname, 'public')))
         const client = await pool.connect()
         const result = await client.query('SELECT * FROM test_table');
         const results = { 'results': (result) ? result.rows : null};
-        res.render('pages/db', results );
+        res.json( results );
         client.release();
       } catch (err) {
         console.error(err);
